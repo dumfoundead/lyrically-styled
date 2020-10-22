e8faa898a45ff214c65d70b3ba50416c06248dd3
dumfoundead <69743095+dumfoundead@users.noreply.github.com>
2020-10-21 22:45:55 -0400

    Update script.js
    added functions to fetch and display data from youtube api

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const apiURL = 'https://api.lyrics.ovh';
+const youTubeApiKey = 'AIzaSyBHJsYZwa3nILNreY3v7LN2gCuBlfX_WeY';
+const lyricsApiURL = 'https://api.lyrics.ovh';
+const youTubeApiURL = 'https://www.googleapis.com/youtube/v3';
 
 const form = $('#form');
 const search = $('#search');
@@ -9,7 +11,7 @@ const more = $('#more');
 
 // Search by song or artist
 function searchSongs(term) {
-  fetch(`${apiURL}/suggest/${term}`)
+  fetch(`${lyricsApiURL}/suggest/${term}`)
     .then(response => response.json())
     .then(data => showData(data))
     .catch((error) => {
@@ -63,17 +65,59 @@ function getMoreSongs(url) {
 
 // Get lyrics for song
 async function getLyrics(artist, songTitle) {
-  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const res = await fetch(`${lyricsApiURL}/v1/${artist}/${songTitle}`);
   const data = await res.json();
   const lyrics = data.lyrics.replaceAll(/(\r\n|\r|\n)/g, '<br>');
 
   $('#result').html(`
   <button id='backBtn' class='btn'>Back</button>
   <h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span><a href='${link}' target='_blank'>${thumbnail}</a></span>
   <span>${lyrics}</span>
   `);
 
   $('#more').html('');
+}
+
+// Format parameters for YouTube
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+// Display results for YouTube
+function getYouTubeResults(responseJson, maxResults) {
+  for (let i = 0; i < maxResults; i++){
+    const link = `https://www.youtube.com/watch?v=${responseJson.items[i].id.videoId}`;
+    const thumbnail = `<img src='${responseJson.items[i].snippet.thumbnails.default.url}'>`;
+};
+
+function getYouTubeVideos(artist, songTitle, maxResults=1) {
+  const params = {
+    key: youTubeApiKey,
+    q: artist+songTitle,
+    part: 'snippet',
+    maxResults,
+    type: 'video'
+  };
+
+  const queryString = formatQueryParams(params)
+  const url = youTubeApiURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => getYouTubeResults(responseJson, maxResults))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 }
 
 // Event Listeners - 'Search' button click
@@ -96,6 +140,7 @@ $('body').on('click', '#getLyricsBtn', function(event) {
     const artist = clickedEl.getAttribute('data-artist');
     const songTitle = clickedEl.getAttribute('data-songtitle');
     getLyrics(artist, songTitle);
+    getYouTubeVideos(artist, songTitle);
   }
 });
 
